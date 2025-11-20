d06d7bb47114688580358ea7eaf79dea17996120
Acha <acha@achalogy.dev>
2025-11-20 12:53:26 -0500

    fallback

--- src/inputExecution/index.ts
+++ src/inputExecution/index.ts
@@ -4,42 +4,80 @@ import type { ZMQSender } from '@acha/distribuidos/zeromq/ZMQSender'
 import ZMQSyncRequest from '../net/ZMQSyncRequest.ts'
 import { OperationType, type BibOperation } from '@acha/distribuidos/schemas/BibOperation'
 import { Config, writeLog } from '@acha/distribuidos'
-import type { ZMQReceiver } from '@acha/distribuidos/zeromq/ZMQReceiver'
 import ZMQSyncReply from '../net/ZMQSyncReply.ts'
+import FallbackZMQSyncRequest from '../net/FallbackZMQSyncRequest.ts'
 
 export default async (op: BibOperation) => {
   const syncSocket: ZMQSender = ZMQSyncRequest.getInstance();
-
-  let replier;
-
-  if (Config.getInstance().get("type") == "reserve") {
-    replier = ZMQSyncReply.getInstance();
-  }
+  const fallbackSyncSocket: ZMQSender = FallbackZMQSyncRequest.getInstance();
 
   process.stdout.write(op.toString());
   process.stdout.write(`${colors.cyan(" ...")}`);
 
   let req;
 
+
   switch (op.getOperation()) {
     case OperationType.RENEW:
-      req = syncSocket.sendRenew({ body: op })
+      if (!Config.getInstance().get("storage_down_time") ||
+        (new Date().getTime() - (Config.getInstance().get("storage_down_time") as Date).getTime()) > 20000) { // Revisa de nuevo en 20 segundos
+        req = await syncSocket.sendRenew({ body: op })
+        if (!req.ok && req.body == "TIMEOUT") {
+          console.log(colors.red("STORAGE MANAGER TIMEOUT"))
+          writeLog("STORAGE MANAGER TIMEOUT")
+          writeLog("SENDING TO FALLBACK STORAGE MANAGER")
+          Config.getInstance().set("storage_down_time", new Date())
+          req = await fallbackSyncSocket.sendRenew({ body: op })
+        }
+      } else {
+        writeLog("SENDING TO FALLBACK STORAGE MANAGER")
+        req = await fallbackSyncSocket.sendRenew({ body: op })
+      }
       break;
     case OperationType.RETURN:
-      req = syncSocket.sendReturn({ body: op })
+      if (!Config.getInstance().get("storage_down_time") ||
+        (new Date().getTime() - (Config.getInstance().get("storage_down_time") as Date).getTime()) > 20000) { // Revisa de nuevo en 20 segundos
+        req = await syncSocket.sendReturn({ body: op })
+        if (!req.ok && req.body == "TIMEOUT") {
+          console.log(colors.red("STORAGE MANAGER TIMEOUT"))
+          writeLog("STORAGE MANAGER TIMEOUT")
+          writeLog("SENDING TO FALLBACK STORAGE MANAGER")
+          Config.getInstance().set("storage_down_time", new Date())
+          req = await fallbackSyncSocket.sendReturn({ body: op })
+        }
+      } else {
+        writeLog("SENDING TO FALLBACK STORAGE MANAGER")
+        req = await fallbackSyncSocket.sendReturn({ body: op })
+      }
       break;
 
-    case OperationType.RESERVE:
-      req = syncSocket.sendReserve({ body: op })
+    case OperationType.RESERVE: {
+      const replier = ZMQSyncReply.getInstance();
+      if (!Config.getInstance().get("storage_down_time") ||
+        (new Date().getTime() - (Config.getInstance().get("storage_down_time") as Date).getTime()) > 20000) { // Revisa de nuevo en 20 segundos
+        req = await syncSocket.sendReserve({ body: op })
+        if (!req.ok && req.body == "TIMEOUT") {
+          console.log(colors.red("STORAGE MANAGER TIMEOUT"))
+          writeLog("STORAGE MANAGER TIMEOUT")
+          writeLog("SENDING TO FALLBACK STORAGE MANAGER")
+          Config.getInstance().set("storage_down_time", new Date())
+          req = await fallbackSyncSocket.sendReserve({ body: op })
+        }
+      } else {
+        writeLog("SENDING TO FALLBACK STORAGE MANAGER")
+        req = await fallbackSyncSocket.sendReserve({ body: op })
+      }
       writeLog("Replying reserve")
-      writeLog(await req)
-      replier.send(await req);
+      writeLog(req)
+
+      replier.send(req);
       break;
+    }
   }
 
-  await req;
-
-  process.stdout.write(colors.green('\x1b[4D done\n'));
+  process.stdout.write(colors.green('\x1b[4D done'));
+  if(!req.ok) process.stdout.write(colors.red(' with errors'));
+  process.stdout.write("\n")
 
 
 }
--- src/net/FallbackZMQSyncRequest.ts
+++ src/net/FallbackZMQSyncRequest.ts
@@ -1,9 +1,9 @@
 import { BaseZMQSyncRequest } from "@acha/distribuidos/zeromq/BaseZMQSyncRequest"
 import { BibOperation, type BibResponse } from "@acha/distribuidos/schemas/BibOperation"
 import "dotenv/config"
-export default class ZMQSyncRequest extends BaseZMQSyncRequest {
-  protected override host = process.env.STORAGE_MANAGER_HOST!;
-  protected override port = process.env.STORAGE_MANAGER_PORT!;
+export default class FallbackZMQSyncRequest extends BaseZMQSyncRequest {
+  protected override host = process.env.FALLBACK_STORAGE_MANAGER_HOST!;
+  protected override port = process.env.FALLBACK_STORAGE_MANAGER_PORT!;
 
   override sendReserve(context: { body: BibOperation, timeout: number }): Promise<BibResponse> {
     return super.resendBody(context)
